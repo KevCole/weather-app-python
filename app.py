75f9db5afae73abf1206c70ba5cd5a05cd4293c1
Kevin C <kevincolejr@outlook.com>
2020-08-04 20:19:25 -0400

    configured ipstack to check ip address to get latitude and longitude

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import requests
 import json
-
+access_key = "21dc354c69fd878156565f73dc03638f"
+response = requests.get("http://api.ipstack.com/check?access_key=" + access_key)
+json_response = response.json()
 secret_key = "1a765ec287af0c5b80d9b6a186eb3ee0"
 
-location_secret_key = "21dc354c69fd878156565f73dc03638f"
+
 
 #http://api.ipstack.com/check? access_key = {location_secret_key}
-
 #response = requests.get("https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid={key}")
 
-response = requests.get("http://api.ipstack.com/check?access_key={location_secret_key} ")
 
-print(response.status_code)
+
+print(json_response["latitude"])
+print(json_response["longitude"])
+
